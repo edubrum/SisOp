2b9f8c63d601dcbaaeed6dd9463e04660b8e7d2e
edubrum <edulbrum@gmail.com>
2018-09-03 12:07:30 -0300

    a

--- ProcessosConcorrentes/fork-tres-filhos.c
+++ ProcessosConcorrentes/fork-tres-filhos.c
@@ -11,13 +11,13 @@ main()
 {
 int i, pid;
 
-  for (i= 1; i < 3; i++) {
+  for (i= 0; i < 3; i++) {
 	   // printf("Eu sou o processo %ld\n", (long) getpid() );
         if (pid = fork()==0) break ;   //  se for filho, sai do for
 	}
   
   // ao sair do for, todos escrevem
-  printf("Eu sou o processo %ld, e meu pai e %ld\n",(long) getpid(),(long) getppid());
+  printf("Eu sou o processo %ld, e meu pai e %ld \n",(long) getpid(),(long) getppid());
 }
 
 
